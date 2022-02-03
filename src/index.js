945f2696d17feb72322ec89f12484bd4185a35bd
Alexey Lagun <alexey.lagun@gmail.com>
2022-02-03 16:02:01 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -37,8 +37,17 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+//console.log(expr.replace(/.{10}/g, word  =>  ((word === '**********') ? ' ' : (MORSE_TABLE[word.replace(/.{2}/g, symbol => MORSE_KEYS[symbol])]))));
+    
 function decode(expr) {
-    // write your solution here
+    const MORSE_KEYS = {
+        '10' : '.', 
+        '11': '-',
+        '00': '',
+    };
+// ----идем по 10 символов строки -- сравниваем с пробелом --- проверям 10 символов по 2 на соответствие с заменяе согласно нашей таблицы и вызываем замену по ключу    
+return expr.replace(/.{10}/g, word  =>  ((word === '**********') ? ' ' : (MORSE_TABLE[word.replace(/.{2}/g, symbol => MORSE_KEYS[symbol])])));  
+      
 }
 
 module.exports = {
